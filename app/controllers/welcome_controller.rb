a1488ec902d1d4222bc7faa504abb2eae6264209
replace <mchalabih@gmail.com>
2017-03-14 13:40:48 -0300

    1000 palabras y primeras 10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @admins = Admin.all.order(:created_at).reverse_order
+    @admins = Admin.all.order(:created_at).reverse_order.first(10)
   end 
   
 end
